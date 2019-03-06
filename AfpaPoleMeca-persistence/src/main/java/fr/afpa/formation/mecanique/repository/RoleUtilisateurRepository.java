443f7db5ae3c3bf6847d51326abe5367950e1980
KsoGaki <khnoll@live.fr>
2019-03-06 16:50:13 +0100

    EnumEntityRepo

---
+++ AfpaPoleMeca-persistence/src/main/java/fr/afpa/formation/mecanique/repository/RoleUtilisateurRepository.java
@@ -0,0 +1,9 @@
+package fr.afpa.formation.mecanique.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.afpa.formation.mecanique.entity.RoleUtilisateur;
+
+public interface RoleUtilisateurRepository extends CrudRepository<RoleUtilisateur, Long>{
+
+}
