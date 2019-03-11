b6318973118077b181888d06924ffd771320b5bc
SaidBG <ajitoshire12@gmail.com>
2019-03-11 13:17:14 +0100

    Lundi 11

---
+++ AfpaPoleMeca-persistence/src/main/java/fr/afpa/formation/mecanique/repository/ClotureRepository.java
@@ -0,0 +1,9 @@
+package fr.afpa.formation.mecanique.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.afpa.formation.mecanique.entity.Cloture;
+
+public interface ClotureRepository extends CrudRepository<Cloture, Long> {
+
+}
