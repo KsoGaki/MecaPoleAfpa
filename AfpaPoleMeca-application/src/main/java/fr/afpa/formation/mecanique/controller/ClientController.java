a8d6eaad4428d945c023be25dc97db690de89997
KsoGaki <khnoll@live.fr>
2019-03-12 17:09:22 +0100

    reboot Client + css

--- AfpaPoleMeca-application/src/main/java/fr/afpa/formation/mecanique/controller/ClientController.java
+++ AfpaPoleMeca-application/src/main/java/fr/afpa/formation/mecanique/controller/ClientController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import fr.afpa.formation.mecanique.service.ClientService;
 
@@ -11,15 +12,20 @@ import fr.afpa.formation.mecanique.service.ClientService;
 public class ClientController {
 	
 	@Autowired
-	ClientService service;
+	ClientService clientService;
 	
 	@GetMapping("/client")
-	public String getClients(Model model) throws Exception {
-		
-		model.addAttribute("clients", service.findAll());
-		
+	public String getClients(Model model) throws Exception {	
+		model.addAttribute("clients", clientService.findAll());	
 		return "client";
 	}
+	
+	@GetMapping("/client/{id}/")
+	public String getClientById(@PathVariable("id") Long id, Model model) throws Exception {	
+		model.addAttribute("clients", clientService.findById(id));	
+		return "fiche_client";
+	}
+	
 	
 
 }
